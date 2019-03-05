f777d997d2c295c7e94f3ad968946e73f3cd74af
jtans <znfox_sz@foxmail.com>
2019-03-05 16:45:04 +0800

    解决关联xib的View或者ViewController时，Project没对应修改类名导致加载不到相应的xib问题

--- CDXibStoryBoardProcessor.m
+++ CDXibStoryBoardProcessor.m
@@ -23,6 +23,31 @@
             // Return YES if the enumeration should continue after the error.
             return YES;
     }];
+    
+    //找到project.pbxproj 替换xib名字为混淆后的名字
+    NSDirectoryEnumerator *enumerator2 = [fileManager
+                                         enumeratorAtURL:directoryURL
+                                         includingPropertiesForKeys:keys
+                                         options:0
+                                         errorHandler:^(NSURL *url, NSError *error) {
+                                             // Handle the error.
+                                             // Return YES if the enumeration should continue after the error.
+                                             return YES;
+                                         }];
+    __block NSURL *pbxprojUrl;
+    for (NSURL *url in enumerator2) {
+        NSError *error;
+        NSNumber *isDirectory = nil;
+        if ([url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:&error] && ![isDirectory boolValue]) {
+            if ([url.absoluteString hasSuffix:@".pbxproj"]) {
+                pbxprojUrl = url;
+                NSLog(@"pbxproj path %@",pbxprojUrl);
+                break;
+            }
+        }
+    }
+    NSString *pbxprojStr = [NSString stringWithContentsOfURL:pbxprojUrl encoding:NSUTF8StringEncoding error:NULL];
+    
 
     CDXibStoryboardParser *parser = [[CDXibStoryboardParser alloc] init];
     for (NSURL *url in enumerator) {
@@ -34,8 +59,8 @@
                 NSData *data = [parser obfuscatedXmlData:[NSData dataWithContentsOfURL:url] symbols:symbols];
                 
                 
-                NSString *srcClsName = [[url lastPathComponent] stringByDeletingPathExtension];
-                NSString *desClsName = symbols[srcClsName];
+                NSString *srcClsName = [[url lastPathComponent] stringByDeletingPathExtension];//混淆前的类名
+                NSString *desClsName = symbols[srcClsName];//混淆后的类名
                 if (desClsName) {
                     NSURL *tempUrl = [[url URLByDeletingLastPathComponent] URLByAppendingPathComponent:[NSString stringWithFormat:@"%@.xib",desClsName]];
                     [data writeToURL:tempUrl atomically:YES];
@@ -47,12 +72,25 @@
                     if (error) {
                         NSLog(@"remove file error :%@",error);
                     }
+                    
+                    
+                    //替换pbxproj 里的xib名字
+                    @autoreleasepool {
+                        pbxprojStr = [pbxprojStr stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@"path = %@.xib",srcClsName] withString:[NSString stringWithFormat:@"path = %@.xib",desClsName]];
+                    }
                 }
                 else {
                     [data writeToURL:url atomically:YES];
                 }
             }
         }
+    }
+    
+    //保存pbxprojStr
+    NSError *error;
+    [pbxprojStr writeToURL:pbxprojUrl atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    if (error) {
+        NSLog(@"save pbxproj error:%@",error);
     }
 }
 
